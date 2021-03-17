bd64771a9727ee9885068a0895391142358dc4a5
ederusena <eder.sena@live.com>
2021-03-17 19:23:08 -0300

    Project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const net = require('net');
 const { getLocationInfos } = require('./location');
 
 const getHeaderValue = (data, header) => {
+  console.log(data, header);
   const headerData = data
     .split('\r\n')
     .find((chunk) => chunk.startsWith(header));
@@ -16,16 +17,29 @@ const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    // console.log('data==>',data.toString());
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
-    getLocationInfos(clientIP, (/* locationData */) => {
+    getLocationInfos(clientIP, (locationData) => {
+      console.log(locationData);
+      const {
+        city,
+        postal_code: postalCode,
+        region,
+        country_name: countryName,
+        company,
+      } = locationData;
       socket.write(startOfResponse);
       socket.write(
         '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
       );
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
+      socket.write(`<H2 data-testid="ip">${clientIP}</H2>`);
+      socket.write(`<H3 data-testid="city">${city}</H3>`);
+      socket.write(`<H3 data-testid="postal_code">${postalCode}</H3>`);
+      socket.write(`<H3 data-testid="region">${region}</H3>`);
+      socket.write(`<H3 data-testid="country">${countryName}</H3>`);
+      socket.write(`<H3 data-testid="company">${company}</H3>`);
       socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
       socket.write('</body></html>');
       socket.write(endOfResponse);
