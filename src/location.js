46a684fba3138aaa1d18846cdcce6eb1517bdd00
ederusena <eder.sena@live.com>
2021-03-17 19:19:03 -0300

    Project

--- src/location.js
+++ src/location.js
@@ -1,7 +1,11 @@
 const https = require('https');
 
 const options = {
-  //
+  hostname: 'iplocation.com',
+  port: 443,
+  path: '/',
+  method: 'POST',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 };
 
 const getLocationInfos = (clientIP, cb) => {
@@ -22,6 +26,7 @@ const getLocationInfos = (clientIP, cb) => {
 
   // TO DO: Enviar mensagem (IP) ao server
 
+  req.write(`ip=${clientIP}`);
   req.end();
 };
 
