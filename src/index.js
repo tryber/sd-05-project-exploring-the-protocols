5f9ae37967ca12a8734ba7a169999a3d3f40013d
Preto115 <preto115@gmail.com>
2021-04-15 01:32:40 -0300

    implementação dos requisitos 4,5 e 6

--- src/index.js
+++ src/index.js
@@ -9,9 +9,12 @@ const getHeaderValue = (data, header) => {
 };
 
 console.log(getHeaderValue());
-const startOfResponse = null;
+const startOfResponse = `${[
+  'HTTP/1.1 200 OK',
+  'Content-Type: text/html; charset=UTF-8',
+].join('\r\n')}`;
 
-const endOfResponse = null;
+const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
--- src/location.js
+++ src/location.js
@@ -1,7 +1,13 @@
 const https = require('https');
 
 const options = {
-  //
+  hostname: 'iplocation.com',
+  port: 443,
+  path: '/',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
 };
 
 const getLocationInfos = (clientIP, cb) => {
