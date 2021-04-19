2c04c0a919562352fd1dbe274eae8e3fd04cfe4c
Alexander Mitchell <alexlm.trybe@gmail.com>
2021-04-19 01:09:14 -0300

    testando o erro dos requisitos depois do 8

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ const net = require('net');
 const { getLocationInfos } = require('./location');
 
 const getHeaderValue = (data, header) => {
-  const headerData = data
-    .split('\r\n')
-    .find((chunk) => chunk.startsWith(header));
+  const headerData = data.split('\r\n').find((chunk) => chunk.startsWith(header));
 
   return headerData.split(': ').pop();
 };
@@ -18,12 +16,22 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
-    getLocationInfos(clientIP, (_locationData) => {
+    getLocationInfos(clientIP, (locationData) => {
       socket.write(startOfResponse);
-      socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
+      socket.write(
+        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
+      );
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
-      socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+      socket.write(
+        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+      );
+      socket.write(`<p data-testid="ip">${clientIP}</p>`);
+      socket.write(`<p data-testid="city">${locationData.city}</p>`);
+      socket.write(`<p data-testid="postal_code">${locationData.postal_code}</p>`);
+      socket.write(`<p data-testid="region">${locationData.region}</p>`);
+      socket.write(`<p data-testid="country">${locationData.country_name}</p>`);
+      socket.write(`<p data-testid="company">${locationData.company}</p>`);
       socket.write('</body></html>');
       socket.write(endOfResponse);
     });
--- src/location.js
+++ src/location.js
@@ -26,6 +26,7 @@ const getLocationInfos = (clientIP, cb) => {
   });
 
   // TO DO: Enviar mensagem (IP) ao server
+  req.write(`ip=${clientIP}`);
 
   req.end();
 };
