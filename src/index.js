ddbbfd88d96915e2aae0610df917a8da9d434aa8
Preto115 <preto115@gmail.com>
2021-04-24 16:02:41 -0300

    requisito 6

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const getHeaderValue = (data, header) => {
   return headerData.split(': ').pop();
 };
 
-const startOfResponse = null;
+const startOfResponse = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n';
 
 const endOfResponse = null;
 
