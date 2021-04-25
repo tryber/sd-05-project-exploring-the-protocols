013f63527acb7b20009c3752ed26dc92c6e8bbd1
rafaelpbar <rafael.coper@outlook.com>
2021-04-24 21:51:37 -0300

    Thanks to Samuca ninja brother

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-/* const net = require('net');
+const net = require('net');
+const os = require('os');
 
 const { getLocationInfos } = require('./location');
 
@@ -17,25 +18,24 @@ const endOfResponse = '\r\n\r\n';
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
+    const deviceInfo = getHeaderValue(data.toString(), 'User-Agent');
 
     getLocationInfos(clientIP, (locationData) => {
       socket.write(startOfResponse);
-      socket.write(
-        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
-      );
+      socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
+      socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
       socket.write(`<p data-testid="ip">${clientIP}</p>`);
+      socket.write(`<p data-testid="postal_code">${locationData.postal_code}</p>`);
       socket.write(`<p data-testid="city">${locationData.city}</p>`);
-      socket.write(
-        `<p data-testid="postal_code">${locationData.postalCode}</p>`,
-      );
       socket.write(`<p data-testid="region">${locationData.region}</p>`);
-      socket.write(`<p data-testid="country">${locationData.country}</p>`);
+      socket.write(`<p data-testid="country">${locationData.country_name}</p>`);
       socket.write(`<p data-testid="company">${locationData.company}</p>`);
-      socket.write(
-        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
-      );
+      socket.write(`<p data-testid="arch">${os.platform() - os.arch() - os.release()}</p>`);
+      socket.write(`<p data-testid="cpu">${os.cpu}</p>`);
+      socket.write(`<p data-testid="memory">${os.memory}</p>`);
+      socket.write(`<p data-testid="device">${deviceInfo}</p>`);
       socket.write('</body></html>');
       socket.write(endOfResponse);
     });
@@ -43,4 +43,3 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(8080);
- */
