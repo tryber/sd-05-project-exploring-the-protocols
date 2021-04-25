4776d20982bd42198d389a5c43383d7af4bb1e8a
Preto115 <preto115@gmail.com>
2021-04-25 02:11:05 -0300

    fazendo pull da master

--- tests/testHttpServer.spec.js
+++ tests/testHttpServer.spec.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
-var execTerminal = require('child_process').exec,
-  child;
+var execTerminal = require('child_process').exec, child;
 
 const BASE_URL = 'http://localhost:8085';
 
@@ -18,10 +17,7 @@ describe('3 - Criar um server TCP utilizando o módulo net capaz de responder co
   let page;
 
   beforeEach(async () => {
-    browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--window-size=1920,1080'],
-      headless: true,
-    });
+    browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--window-size=1920,1080'], headless: true });
     page = await browser.newPage();
   });
 
@@ -31,13 +27,13 @@ describe('3 - Criar um server TCP utilizando o módulo net capaz de responder co
 
   it('Será validado que ao fazer a request e acessar a url irá mostrar o texto "Protocolos"', async () => {
     var execNode = execTerminal('node exploiters/httpServer.js');
-    execNode.stdout.on('data', () => {});
+    execNode.stdout.on('data', ()=>{ });
 
     wait(2000);
 
     await page.goto(BASE_URL);
 
-    const text = await page.$$eval('h1', (nodes) => nodes.map((n) => n.innerText));
+    const text =  await page.$$eval('h1', (nodes) => nodes.map((n) => n.innerText));
     expect(text).toContain('Protocolos');
 
     execNode.kill();
