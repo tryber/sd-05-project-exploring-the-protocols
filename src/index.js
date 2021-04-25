d1d644517c087851df2413a9f26a81fb3e1aa5a6
luialbeto <araujo.script@gmail.com>
2021-04-25 11:57:47 -0300

    fix lint

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const net = require('net');
 
 const { getLocationInfos } = require('./location');
 
-const getHeaderValue = (data, header) => {
+/* const getHeaderValue = (data, header) => {
   const headerData = data
     .split('\r\n')
     .find((chunk) => chunk.startsWith(header));
 
   return headerData.split(': ').pop();
-};
+}; */
 
 const startOfResponse = null;
 
@@ -17,13 +17,19 @@ const endOfResponse = null;
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const clientIP = null;
+    console.log(data);
 
     getLocationInfos(clientIP, (locationData) => {
+      console.log(locationData);
       socket.write(startOfResponse);
-      socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
+      socket.write(
+        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">'
+      );
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
-      socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+      socket.write(
+        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
+      );
       socket.write('</body></html>');
       socket.write(endOfResponse);
     });
