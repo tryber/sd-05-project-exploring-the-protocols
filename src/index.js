25d8c3a1e7f67f1626cb8f8fee71a718f68959b9
luialbeto <araujo.script@gmail.com>
2021-04-25 12:01:18 -0300

    fix lint 2

--- src/index.js
+++ src/index.js
@@ -23,12 +23,12 @@ const server = net.createServer((socket) => {
       console.log(locationData);
       socket.write(startOfResponse);
       socket.write(
-        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">'
+        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
       );
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
       socket.write(
-        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
+        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
       );
       socket.write('</body></html>');
       socket.write(endOfResponse);
